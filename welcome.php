6ce94d33c6bc838e0403abacb8ee13e821557139
thomhie0416 <48708193+thomhie0416@users.noreply.github.com>
2019-11-21 21:20:13 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hello World</title>
+	<center>
 </head>
-<body>
-	<?php  echo" HELLO WORLD" ;?>
-	<?php //this is comment only //?>
-	<a href="index.php">welcome </a>
+<body style="background-image: url(pic1.jpg);background-repeat: no-repeat;background-size: cover;">
+<h1>
 
+	<?php  echo" THOMMY CALLEDO" ;?>
+	</h1>
+	<div style="background-color:transparent; border-radius:12px; border: 2px solid blue; text-decoration: none; height: 400px; width: 400px;">
+	<?php //this is comment only //?>
+	<br><br><br><a href="index.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Welcome</button> </a><br>
+	<a href="getmethod.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Getmethod</button> </a><br>
+	<a href="Welcome.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Displaymethod</button> </a><br>
+	<a href="post.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Post</button> </a><br>
+	<a href="post.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >String</button> </a><br>
+	<a href="post.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >POSTMethod</button> </a><br>
+
+
+
+
+	</div>
+	<a href="index.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" ></button></a>
+</center>
 </body>
 </html>
