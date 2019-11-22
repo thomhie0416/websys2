406e2d222e26c2bcde45c4ac83d4533ce7962144
thomhie0416 <48708193+thomhie0416@users.noreply.github.com>
2019-11-21 21:19:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
+<center>
 	<title>Hello World</title>
 </head>
 <body>
+<div style="background-position: center;background-image:url(pic2.jpg);background-repeat: no-repeat;background-size: cover; border-radius: 12px;  border: 5px solid black; text-decoration: none; height: 500px; width: 500px">
+<h1>
+
 	<?php  echo "Hello,  World!";?>
-	<a href="welcome.php">home </a>
+	</h1>
+	</div>
+	<a href="welcome.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;">home</button></a>
+	</center>
 </body>
 </html>
